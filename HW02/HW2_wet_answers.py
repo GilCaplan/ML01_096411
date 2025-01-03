4c2029dda3d4082e01867041cd1bf628f67de7f7
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2025-01-03 15:42:18 +0200

    Add files via upload
    final solutions and including instruction for coursework homework spring 2024 course 096411

---
+++ HW02/HW2_wet_answers.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import argparse
+import time
+import itertools
+import numpy as np
+import pandas as pd
+
+
+class PerceptronClassifier:
+    def __init__(self):
+        """
+        Constructor for the PerceptronClassifier.
+        """
+        self.w = np.array([])
+        self.ids = (337604821, 340915156)
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        This method trains a multiclass perceptron classifier on a given training set X with label set y.
+        :param X: A 2-dimensional numpy array of m rows and d columns. It is guaranteed that m >= 1 and d >= 1.
+        Array datatype is guaranteed to be np.float32.
+        :param y: A 1-dimensional numpy array of m rows. it is guaranteed to match X's rows in length (|m_x| == |m_y|).
+        Array datatype is guaranteed to be np.uint8.
+        """
+        K = np.unique(y).shape[0]
+        w = np.array([np.zeros(X.shape[1]) for _ in range(K)])
+        while True:
+            prev = np.array([w_i for w_i in w])
+            for t in range(X.shape[0]):
+                y_t = np.argmax(np.dot(w, X[t]))
+                if y[t] != y_t:
+                    w[y_t] = w[y_t] - X[t]
+                    w[y[t]] = w[y[t]] + X[t]
+            if np.array_equal(prev, w):
+                self.w = np.array([w_i for w_i in w])
+                break
+
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        This method predicts the y labels of a given dataset X, based on a previous training of the model.
+        It is mandatory to call PerceptronClassifier.fit before calling this method.
+        :param X: A 2-dimensional numpy array of m rows and d columns. It is guaranteed that m >= 1 and d >= 1.
+        Array datatype is guaranteed to be np.float32.
+        :return: A 1-dimensional numpy array of m rows. Should be of datatype np.uint8.
+        """
+        return np.array([np.argmax(np.dot(self.w, X[i])) for i in range(len(X))])
+
+
+if __name__ == "__main__":
+
+    print("*" * 20)
+    print("Started HW2_ID1_ID2.py")
+    # Parsing script arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv', type=str, help='Input csv file path')
+    args = parser.parse_args()
+
+    print("Processed input arguments:")
+    print(f"csv = {args.csv}")
+
+    print("Initiating PerceptronClassifier")
+    model = PerceptronClassifier()
+    print(f"Student IDs: {model.ids}")
+    print(f"Loading data from {args.csv}...")
+    data = pd.read_csv(args.csv, header=None)
+    print(f"Loaded {data.shape[0]} rows and {data.shape[1]} columns")
+    X = data[data.columns[:-1]].values.astype(np.float32)
+    y = pd.factorize(data[data.columns[-1]])[0].astype(np.uint8)
+
+    print("Fitting...")
+    is_separable = model.fit(X, y)
+    print("Done")
+    y_pred = model.predict(X)
+    print("Done")
+    accuracy = np.sum(y_pred == y.ravel()) / y.shape[0]
+    print(f"Train accuracy: {accuracy * 100 :.2f}%")
+
+    print("*" * 20)
